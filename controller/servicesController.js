adc253a3da9e452ed3e71dd4b3cac272b32edeff
Diogenes Pereira <diogenes.pereira@luizalabs.com>
2022-02-09 22:00:25 -0300

    feat: router queries and find

--- controller/servicesController.js
+++ controller/servicesController.js
@@ -1,6 +1,8 @@
 const { uuid } = require('uuidv4');
 const fs = require('fs');
 const db = require('../database/models');
+const { sequelize } = require('../database/models');
+const { QueryTypes } = require('sequelize');
 
 const services = {
     index: async (req, res) => {
@@ -16,17 +18,68 @@ const services = {
             servicesList            
         });
     },
-    create: (req, res) => {
-      const { nome, preco, descricao } = req.body;
-  
-      db.Servico.create({
-        uuid: uuid(),
-        nome,
-        preco,
-        descricao,
+    queries: async(req, res) => {
+      const { campo } = req.body;
+      
+      if (campo === 'nome'){
+        const resultadoQuery = await sequelize.query(
+          "SELECT * FROM 'services' WHERE name = ?",
+          {
+            replacements : ['b','o']
+          }
+        )
+        res.status(201).json(resultadoQuery)
+      }else if (campo === 'preco'){
+        const resultadoQuery = await sequelize.query(
+          "SELECT * FROM 'services' WHERE preco > :price",
+          {
+            replacements : {price: 200}
+          }
+        )
+        res.status(201).json(resultadoQuery)
+      }else if (campo === 'descricao'){
+        const resultadoQuery = await sequelize.query(
+          "SELECT * FROM 'services' WHERE text LIKE :descricao",
+          {
+            replacements : {descricao: 'pet'}
+          }
+        )
+        res.status(201).json(resultadoQuery)
+      }else if (campo === 'categoria'){
+        const resultadoQuery = await sequelize.query(
+          "SELECT * FROM 'services' s INNER JOIN categoria c ON s.categoria_id = c.id " + 
+        " WHERE c.name LIKE $categoria",
+          {
+            bind : {categoria: 'Alimento'}
+          }
+        )
+        res.status(201).json(resultadoQuery)
+      }
+    },
+    findByPrice: async(req, res) => {
+      const { price } = req.body;
+      const resultadoPreco = await sequelize.query("SELECT * FROM 'services' WHERE price >= $priceVal ",
+      {
+        
+        bind: [{priceVal: price}],
+        type: QueryTypes.SELECT
       })
+      res.status(201).json(resultadoPreco);
+    },
+    create: (req, res) => {
+      const { name, price, text, email, categoria_id } = req.body;
+      let id = uuid();
+      let novoServico = {
+        uuid: id,
+        name,
+        price,
+        text,
+        email,
+        categoria_id
+      }
+      db.Service.create(novoServico)
         .then((result) => {
-          res.status(201).json(resut);
+          res.status(201).send('OK')
         })
         .catch((e) => {
           res.status(500);
--- routes/services.js
+++ routes/services.js
@@ -4,5 +4,8 @@ const servicesController = require('../controller/servicesController')
 
 
 router.get('/', servicesController.index);
+router.post('/create', servicesController.create);
+router.post('/findByPrice', servicesController.findByPrice);
+router.post('/queries', servicesController.queries);
 
 module.exports = router;
